4a8d84f0f2ec4f7b08c2eebfd256bfe392a80f85
Joseph Holten <joseph@holten.com>
2024-09-06 14:26:38 -0400

    update test to actual example

--- test_package/test_bigint.cpp
+++ test_package/test_bigint.cpp
@@ -2,6 +2,17 @@
 #include <fmt/core.h>
 
 int main() {
-    fmt::println("TODO: functionality test bigint");
-    return 0;
+    static constexpr size_t N = 1000;
+
+    jlib::BigInt Prev = 1;
+    jlib::BigInt F = 1;
+
+    size_t i = 2;
+    for(; F.digits().size() < N; i++) {
+        jlib::BigInt Next = F + Prev;
+        Prev = F;
+        F = Next;
+    }
+
+    fmt::println("{}", i);
 }
