60623bef50d9d592e2b0ea9149382d324049fc6f
Joseph Holten <joseph@holten.com>
2024-09-06 15:34:21 -0400

    revert

--- conanfile.py
+++ conanfile.py
@@ -64,6 +64,4 @@ class jlib(ConanFile):
 
     def package_info(self):
         self.cpp_info.components["jassert"].libs = ["jassert"]
-        self.cpp_info.components["jassert"].set_property("cmake_target_name", "jlib::jassert")
         self.cpp_info.components["bigint"].libs = ["bigint"]
-        self.cpp_info.components["bigint"].set_property("cmake_target_name", "jlib::bigint")
